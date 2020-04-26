8557cf737d1c933524dc613b499df9cc526cdb28
xtao17 <xtao17@stlawu.edu>
2020-04-26 15:26:41 -0400

    4.26

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+// Server for a course catalog.
 const express = require('express');
 const session = require('express-session');
 const router = require('./router');
 const connect = require('./db');
 
+// Connect to the database
 connect();
 
 // Create the server
 const app = express();
-app.set('view engine','ejs');
-app.set('views','./views');
 
-//p2--
+// Configure the views
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 // Parse request bodies like query strings
 app.use(express.urlencoded({extended: false}));
 
@@ -20,8 +23,7 @@ app.use(session({
   secret: 'temporary', // Password for client cookies
   resave: false, // Recommended setting
   saveUninitialized: false // Recommended setting
-}));//--
-
+}));
 
 // Ignore icon requests
 app.get('/favicon.ico', function(request, response) {
@@ -40,7 +42,6 @@ app.use(function(request, response, next) {
 app.get('/', function(request, response) {
   response.redirect('/statistics');
 });
-
 
 // Enter admin mode and return to the previous page
 app.get('/login', function(request, response) {
@@ -67,6 +68,43 @@ app.use('/', router);
 app.use(function(request, response) {
   console.log('Responded with 404');
   response.status(404).end();
+});
+
+// Handle duplicate ID errors
+app.use(function(error, request, response, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    console.log('Validation error: Duplicate ID');
+    response.status(400).send('Duplicate ID');
+  } else {
+    next(error);
+  }
+});
+
+// Handle cast errors
+app.use(function(error, request, response, next) {
+  if (error.name === 'CastError') {
+    console.log('Validation error:', error.message);
+    const start = error.message.indexOf('path') + 6;
+    const stop = error.message.length - 1;
+    const field = error.message.substring(start, stop);
+    response.status(400).send(`Invalid ${field}`);
+  } else {
+    next(error);
+  }
+});
+
+// Handle validation errors
+app.use(function(error, request, response, next) {
+  if (error.name === 'ValidationError') {
+    const messages = [];
+    for (const field in error.errors) {
+      console.log('Validation error:', error.errors[field].message);
+      messages.push(error.errors[field].message);
+    }
+    response.status(400).send(messages.join('\n'));
+  } else {
+    next(error);
+  }
 });
 
 // Handle other errors
