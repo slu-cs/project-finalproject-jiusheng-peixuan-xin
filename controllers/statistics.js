1edfa9910c0016fa639fa85d4ac4d511bc5af6a8
PeixuanC <49528679+PeixuanC@users.noreply.github.com>
2020-04-26 15:36:30 -0400

    sort (attempr

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -23,3 +23,38 @@ module.exports.retrieve = function(request, response, next) {
     }
   }).catch(error => next(error));
 };
+
+const Section = require('../models/statistic');
+
+// GET /sections?sort=
+module.exports.index = function(request, response, next) {
+  const order = request.query.sort || 'statistic'; // Default to sort by course
+
+  Section.find().sort(order)
+    .then(sections => response.render('statistic/index', {statistic: statistic, order: order}))
+    .catch(error => next(error));
+};
+
+
+
+//p2
+// POST /statistics (with the new statistic in the request body)
+module.exports.create = function(request, response, next) {
+  statistic.create(request.body)
+    .then(statistic => response.status(201).send(statistic.id))
+    .catch(error => next(error));
+};
+
+// DELETE /statistics/:id
+module.exports.delete = function(request, response, next) {
+  statistic.findByIdAndDelete(request.params.id)
+    .then(statistic => statistic ? response.status(200).end() : next())
+    .catch(error => next(error));
+};
+
+// PUT /statistics/:id (with the changes in the request body)
+module.exports.update = function(request, response, next) {
+  statistic.findByIdAndUpdate(request.params.id, request.body)
+    .then(statistic => statistic ? response.status(200).end() : next())
+    .catch(error => next(error));
+};
