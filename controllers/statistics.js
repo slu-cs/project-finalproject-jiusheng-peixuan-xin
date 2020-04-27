6f220deeff3ac793a4134101cb12c338e5f93dcd
jzhan18 <mizar5528@gmail.com>
2020-04-27 15:45:22 -0400

    structure change

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -2,34 +2,19 @@ const Statistic = require('../models/statistic');
 
 module.exports.index = function(request, response, next) {
   Statistic.distinct('_id')
-    .then(statisticID => response.redirect(`/statistics/${statisticID[0]}`))
+    .then(statisticIDs => response.redirect(`/statistics/${statisticIDs[0]}`))
     .catch(error => next(error));
  };
 
-/*
- module.exports.index = function(request, response, next) {
-   const order = request.query.sort || 'day'; // Default to sort by date
-
-   const queries = [
-     Statistic.find().sort(order),
-     Statistic.distinct('_id')
-   ];
-
-   Promise.all(queries).then(function([statistics, statisticID]) {
-     response.render('statistics/index', {statistics: statistics, order: order, statisticID: statisticID});
-   }).catch(error => next(error));
- };
-*/
-// GET /counties/:id
 module.exports.retrieve = function(request, response, next) {
   const queries = [
     Statistic.findById(request.params.id),
     Statistic.distinct('_id')
   ];
 
-  Promise.all(queries).then(function([statistic, statisticID]) {
+  Promise.all(queries).then(function([statistic, statisticIDs]) {
     if (statistic) {
-      response.render('statistics/index', {statistic: statistic, statisticID: statisticID});
+      response.render('statistics/index', {statistic: statistic, statisticIDs: statisticIDs});
     } else {
       next();
     }
